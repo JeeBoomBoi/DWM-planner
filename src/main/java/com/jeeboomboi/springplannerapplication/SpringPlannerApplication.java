3ca9b229b90d19fb1d42549b7d8e218e4985c9c4
JeeBoomBoi <jeeboxical@gmail.com>
2024-05-01 12:34:15 +0530

    Initial commit

---
+++ src/main/java/com/jeeboomboi/springplannerapplication/SpringPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.jeeboomboi.springplannerapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeeboomboi/springplannerapplication/SpringPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeeboomboi.springplannerapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
