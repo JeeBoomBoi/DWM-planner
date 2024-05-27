3a80c2f8d4195b904004e0e2cbee268ac36b65e9
JeeBoomBoi <jeeboxical@gmail.com>
2024-05-27 20:16:46 +0530

    Adds a completed items page

--- src/main/java/com/jeeboomboi/springplannerapplication/repositories/ToDoItemRepository.java
+++ src/main/java/com/jeeboomboi/springplannerapplication/repositories/ToDoItemRepository.java
@@ -1,9 +1,11 @@
 package com.jeeboomboi.springplannerapplication.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.jeeboomboi.springplannerapplication.models.ToDoItem;
 
 public interface ToDoItemRepository extends CrudRepository<ToDoItem, Long> {
-    
+    List<ToDoItem> findAllByComplete(boolean complete);
 }
